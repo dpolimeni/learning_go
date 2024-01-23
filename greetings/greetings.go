1f94a2454484721afac718174fc851d6bd3fc9ee
dpolimeni <diego.polimeni99@gmail.com>
2024-01-23 08:53:02 +0100

    refactoring modules

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// hello function
-func Hello(name string) string {
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
